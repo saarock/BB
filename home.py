e50c3ab11a3ab91c1641f3dc6f8a4da06eb14839
Aayush <saarock4646@gmail.com>
2023-06-02 12:47:38 +0545

    Added

---
+++ home.py
@@ -0,0 +1,20 @@
+from tkinter import*
+from PIL import Image,ImageTk
+from tkinter import messagebox
+import sys
+import os
+import pickle
+from database import *
+sys.path.insert(1,"C:\\Users\\DELL\\Desktop\\TkProject\\1")
+
+
+window = Tk()
+window.title("Home")
+window.geometry("1000x500")
+window.resizable(False,False)
+
+
+
+
+
+window.mainloop()
